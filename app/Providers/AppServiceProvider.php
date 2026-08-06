5089e744926b6993db4a18dc9ecc0b21f61d3a7c
Muhammad Kahfi <muhammad.kahfi@iconpln.co.id>
2026-08-06 16:50:59 +0700

    Fix local HTTP serve compatibility for 127.0.0.1:8000

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production' || request()->header('X-Forwarded-Proto') === 'https') {
+        if (request()->header('X-Forwarded-Proto') === 'https') {
             URL::forceScheme('https');
         }
     }
